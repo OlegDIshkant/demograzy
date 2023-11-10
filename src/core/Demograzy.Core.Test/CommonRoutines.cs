4d10b3d971677fd7b074acf1880a3e91d35a9323
Oleg Dishkant <ol.dishkant@gmail.com>
2023-11-10 12:04:18 +0000

    Unit tests work with SQLIte db.

--- src/core/Demograzy.Core.Test/CommonRoutines.cs
+++ src/core/Demograzy.Core.Test/CommonRoutines.cs
@@ -1,4 +1,6 @@
+using System.Diagnostics;
 using Demograzy.BusinessLogic;
+using NUnit.Framework.Constraints;
 
 namespace Demograzy.Core.Test
 {
@@ -7,12 +9,55 @@ namespace Demograzy.Core.Test
 
         internal static MainService PrepareMainService()
         {
-            return 
+            const string dbName = "/workspaces/demograzy/src/core/Demograzy.Core.Test/test_db/~$_unit_test_db";
+
+            var testableService = 
                 new Demograzy.BusinessLogic.MainService(
                     new Demograzy.DataAccess.Sql.TransactionMeansFactory(
-                        null /* TODO: put test db here... */));
+                       Demograzy.DataAccess.Sql.SQLite.SqlCommandBuilderFactory.Create(dbName)));
+
+            ApplyAllMigrations(dbName);
+
+            return testableService;
         }
 
+
+        private static void ApplyAllMigrations(string pathToTestDb)
+        {
+            var changelogDirectory = "/workspaces/demograzy/src/core/Demograzy.DataAccess.Sql/db_version_control/changelog/";
+            var changelogRootFile = "changelog-root.json";
+            
+            Directory.SetCurrentDirectory(changelogDirectory);
+
+            var psi = new ProcessStartInfo()
+            {
+                FileName = "liquibase",
+                Arguments = $"update --changelog-file=\"{changelogRootFile}\" --url=jdbc:sqlite:{pathToTestDb}",
+                UseShellExecute = false,
+                RedirectStandardOutput = true,
+                RedirectStandardError = true
+            };
+
+            using (var prc = Process.Start(psi))
+            {
+                prc.WaitForExit();
+                if (prc.ExitCode != 0)
+                {
+                    var output = prc.StandardOutput.ReadToEnd();
+                    var error = prc.StandardError.ReadToEnd();
+
+                    var errMsg = 
+                        "Could not apply migration to in-memory database due to some error.\n\n" +
+                        $"OUTPUT:\n {output}\n\n" + 
+                        $"ERROR:\n {error}";
+
+                    throw new InvalidProgramException(errMsg);
+                }
+            }
+        }
+
+
+        
 
 
     }
