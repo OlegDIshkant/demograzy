b79aca8e1f46b36100fcf239e4a28fa11f35f4b5
Oleg Dishkant <ol.dishkant@gmail.com>
2023-12-10 20:34:38 +0000

    New requirements for voting functionality.

---
+++ src/core/Demograzy.Core.Test/Versus/Vote/Success/CompletingVersusWithoutClearWinner.cs
@@ -0,0 +1,122 @@
+using Demograzy.Core.Test.CommonRoutines;
+using static Demograzy.Core.Test.GeneralConstants;
+using static Demograzy.BusinessLogic.DataAccess.VersusInfo;
+
+namespace Demograzy.Core.Test.Versus.Vote.Success
+{
+    
+    [TestFixture]
+    public class CompletingVersusWithoutClearWinner
+    {
+
+        [TestCase(true)]
+        [TestCase(false)]
+        [Timeout(STANDARD_TIMEOUT)]
+        public async Task WhenVotersVoteEvenlyThenWinnerEntryInVersusInfoDefinedByTheLastVote(bool lastVoteForFirst)
+        {     
+            var service = StartUpRoutines.PrepareMainService();
+            // Add room
+            var (room, owner) = await service.NewRoomAndOwner();
+            // Add extra room members
+            var extraMember1 = await service.NewExtraMember(room);
+            var extraMember2 = await service.NewExtraMember(room);
+            var extraMember3 = await service.NewExtraMember(room);
+            // Add candidates
+            var candidate1 = (await service.AddCandidateAsync(room, "candidate1")).Value;
+            var candidate2 = (await service.AddCandidateAsync(room, "candidate2")).Value;
+            // Start voting
+            Assert.That(await service.StartVotingAsync(room));
+            // Get versus
+            var versus = (await service.GetActiveVersesAsync(room, owner)).Single();
+
+            // Complete versus!
+            // Votes should be distributed equally between candidates. 
+            if (lastVoteForFirst)
+            {
+                Assert.That(await service.VoteAsync(versus, extraMember1, votedForFirst: false));
+                Assert.That(await service.VoteAsync(versus, extraMember2, votedForFirst: false));
+                Assert.That(await service.VoteAsync(versus, owner, votedForFirst: true));
+                Assert.That(await service.VoteAsync(versus, extraMember3, votedForFirst: true));
+            }
+            else
+            {
+                Assert.That(await service.VoteAsync(versus, extraMember1, votedForFirst: true));
+                Assert.That(await service.VoteAsync(versus, extraMember2, votedForFirst: true));
+                Assert.That(await service.VoteAsync(versus, owner, votedForFirst: false));
+                Assert.That(await service.VoteAsync(versus, extraMember3, votedForFirst: false));
+            }
+
+
+            // Check
+            var status = (await service.GetVersusInfoAsync(versus)).Value.status;
+
+            if (lastVoteForFirst) 
+                Assert.That(status == Statuses.FIRST_WON);
+            else 
+                Assert.That(status == Statuses.SECOND_WON);
+
+        }
+
+
+
+
+        [TestCase(true)]
+        [TestCase(false)]
+        [Timeout(STANDARD_TIMEOUT)]
+        public async Task WhenVotersVoteEvenlyThenCandidateChosenByLastVoteGoesToNextVersus(bool lastVoteForFirst)
+        {     
+
+
+            var service = StartUpRoutines.PrepareMainService();
+            // Add room
+            var (room, owner) = await service.NewRoomAndOwner();
+            // Add extra room members
+            var extraMember1 = await service.NewExtraMember(room);
+            var extraMember2 = await service.NewExtraMember(room);
+            var extraMember3 = await service.NewExtraMember(room);
+            // Add candidates
+            var candidate1 = (await service.AddCandidateAsync(room, "candidate1")).Value;
+            var candidate2 = (await service.AddCandidateAsync(room, "candidate2")).Value;
+            var candidate3 = (await service.AddCandidateAsync(room, "candidate3")).Value;
+            // Start voting
+            Assert.That(await service.StartVotingAsync(room));
+            // Get versus
+            var versus = (await service.GetActiveVersesAsync(room, owner)).Single();
+            var versusInfo = (await service.GetVersusInfoAsync(versus)).Value;
+            var expectedNextVersusCandidates = new List<int>()
+            {
+                candidate3,
+                lastVoteForFirst ? versusInfo.firstCandidateId : versusInfo.secondCandidateId  
+            };
+
+            // Complete versus!
+            // Votes should be distributed equally between candidates. 
+            if (lastVoteForFirst)
+            {
+                Assert.That(await service.VoteAsync(versus, extraMember1, votedForFirst: false));
+                Assert.That(await service.VoteAsync(versus, extraMember2, votedForFirst: false));
+                Assert.That(await service.VoteAsync(versus, owner, votedForFirst: true));
+                Assert.That(await service.VoteAsync(versus, extraMember3, votedForFirst: true));
+            }
+            else
+            {
+                Assert.That(await service.VoteAsync(versus, extraMember1, votedForFirst: true));
+                Assert.That(await service.VoteAsync(versus, extraMember2, votedForFirst: true));
+                Assert.That(await service.VoteAsync(versus, owner, votedForFirst: false));
+                Assert.That(await service.VoteAsync(versus, extraMember3, votedForFirst: false));
+            }
+
+
+            // Check
+            var nextVersus = (await service.GetActiveVersesAsync(room, owner)).Single();
+            var nextVersusInfo = (await service.GetVersusInfoAsync(versus)).Value;
+            var nextVersusCandidates = new List<int>() 
+            { 
+                nextVersusInfo.firstCandidateId,
+                nextVersusInfo.secondCandidateId
+            };
+            Assert.That(nextVersusCandidates, Is.EquivalentTo(expectedNextVersusCandidates));
+        }
+
+    }
+}
